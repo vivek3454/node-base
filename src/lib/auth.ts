237d235a295976d960419081e3ec9f1017aef7e0
vivek3454 <vivekparde2411@gmail.com>
2025-11-01 17:17:15 +0530

    ✨ chore(dependencies): add polar-sh dependencies
    - add @polar-sh/better-auth and @polar-sh/sdk for authentication and subscription management
    
    ✨ feat(auth): integrate polar for subscription management
    
    - add polar plugin to better-auth for subscription management
    - configure checkout, portal, and usage plugins
    - create customer on signup
    
    ✨ feat(subscription): add subscription hooks
    
    - add useSubscription and useHasActiveSubscription hooks to fetch and manage user subscriptions
    
    ✨ feat(sidebar): add pro upgrade button
    
    - add upgrade to pro button in sidebar, redirect to polar checkout
    
    ✨ feat(trpc): add premium procedure
    
    - add premiumProcedure to trpc to check for active subscription before accessing premium features
    - add polarClient to trpc context
    
    🐛 fix(sidebar): fix upgrade button condition
    
    - fix: display upgrade button only when user does not have active subscription and is not loading
    
    ♻️ refactor(auth): use polarClient from lib
    
    - refactor: use polarClient from lib/polar.ts instead of creating new instance
    
    ✨ feat(polar): add polar client
    
    - add polar client for interacting with polar api

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,6 +1,16 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import prisma from "./db";
+import {
+  polar,
+  checkout,
+  portal,
+  usage,
+  webhooks,
+} from "@polar-sh/better-auth";
+import { Polar } from "@polar-sh/sdk";
+import { polarClient } from "./polar";
+
 
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
@@ -8,6 +18,25 @@ export const auth = betterAuth({
   }),
   emailAndPassword: {
     enabled: true,
-    autoSignIn:true
+    autoSignIn: true,
   },
+  plugins: [
+    polar({
+      client: polarClient,
+      createCustomerOnSignUp: true,
+      use: [
+        checkout({
+          products: [
+            {
+              productId: "2901d383-ec6b-4d35-be6c-da775a527dc2",
+              slug: "pro",
+            },
+          ],
+          successUrl: process.env.POLAR_SUCCESS_URL,
+          authenticatedUsersOnly: true,
+        }),
+        portal()
+      ],
+    }),
+  ],
 });
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,12 +1,12 @@
 import { z } from "zod";
-import { baseProcedure, createTRPCRouter, protectedProcedure } from "../init";
+import { baseProcedure, createTRPCRouter, premiumProcedure, protectedProcedure } from "../init";
 import prisma from "@/lib/db";
 import { inngest } from "@/inngest/client";
 import { google } from "@ai-sdk/google";
 import { generateText } from "ai";
 
 export const appRouter = createTRPCRouter({
-  testAi: protectedProcedure.mutation(async () => {
+  testAi: premiumProcedure.mutation(async () => {
     await inngest.send({
       name: "execute/ai",
     });
